c605251e1321f093794af4dcb896111878388698
Alex Lundgren <alex@endem.su>
2023-03-23 13:56:22 +0800

    : README.md

---
+++ get_data_from_indexer.py
@@ -0,0 +1,107 @@
+"""gql schema: https://github.com/graphprotocol/graph-node/blob/master/server/index-node/src/schema.graphql"""
+
+import typing as tp
+import time
+import requests
+from pydantic import BaseModel
+import json
+
+BASE_URL = 'http://localhost:8030/graphql'
+
+BASE_QUERY = """
+query {
+        indexingStatuses (limit: 100) {
+            subgraph
+                node
+                entityCount
+                health
+                synced
+                chains {
+                    network
+                    chainHeadBlock {
+                        number
+                    }
+                    latestBlock {
+                        number
+                    }
+                }
+                fatalError {
+                    handler
+                    message
+                    block {
+                        number
+                    }
+                }
+        }
+}
+"""
+
+
+class SubgraphError(BaseModel):
+    handler: tp.Union[str, None]
+    message: str
+    block_number: int
+
+
+class SubgraphIndexingStatus(BaseModel):
+    name: str
+    health: str
+    synced: bool
+    hash: str
+    node: tp.Union[str, None]
+    network: str
+    head_block: int
+    latest_block: int
+    entities: int
+    error: tp.Union[SubgraphError, None]
+
+
+class SubgraphsIndexingResult(BaseModel):
+    check_date: int = int(time.time())
+    subgraphs: tp.List[SubgraphIndexingStatus]
+    length: int
+
+
+class SubgraphsStat(BaseModel):
+    data: tp.List[tp.Optional[SubgraphsIndexingResult]] = []
+
+
+result = requests.post(
+    BASE_URL, json={'query': BASE_QUERY, 'variables': None})
+
+subgraphs = []
+subgraph_indexing_data = result.json()['data']['indexingStatuses']
+for subgraph in subgraph_indexing_data:
+    if subgraph['fatalError']:
+        subgraph_error = SubgraphError(
+            block_number=subgraph['fatalError']['block']['number'],
+            handler=subgraph['fatalError']['handler'],
+            message=subgraph['fatalError']['message'])
+    else:
+        subgraph_error = None
+
+    subgraph_status = SubgraphIndexingStatus(
+        name='XXX',  # TODO: Get the Name
+        hash=subgraph['subgraph'],
+        health=subgraph['health'],
+        synced=subgraph['synced'],
+        entities=subgraph['entityCount'],
+        head_block=subgraph['chains'][0]['chainHeadBlock']['number'],
+        latest_block=subgraph['chains'][0]['latestBlock']['number'],
+        network=subgraph['chains'][0]['network'],
+        node=subgraph['node'],
+        error=subgraph_error,
+    )
+    subgraphs.append(subgraph_status)
+
+result = SubgraphsIndexingResult(subgraphs=subgraphs, length=len(subgraphs))
+
+with open('./checks.json', 'r') as f:
+    try:
+        stats_data = SubgraphsStat(**json.loads(f.read()))
+    except json.decoder.JSONDecodeError:
+        stats_data = SubgraphsStat(data=[])
+
+with open('./checks.json', 'w') as f:
+    stats_data.data.append(result)
+    f.write(stats_data.json())
--- get_stats.py
+++ get_stats.py
@@ -1,40 +1,8 @@
-"""gql schema: https://github.com/graphprotocol/graph-node/blob/master/server/index-node/src/schema.graphql"""
-
-import typing as tp
-import time
-import requests
-from pydantic import BaseModel
 import json
+import typing as tp
+from pydantic import BaseModel
 
-BASE_URL = 'http://localhost:8030/graphql'
-
-BASE_QUERY = """
-query {
-        indexingStatuses (limit: 100) {
-            subgraph
-                node
-                entityCount
-                health
-                synced
-                chains {
-                    network
-                    chainHeadBlock {
-                        number
-                    }
-                    latestBlock {
-                        number
-                    }
-                }
-                fatalError {
-                    handler
-                    message
-                    block {
-                        number
-                    }
-                }
-        }
-}
-"""
+subgraphs_stat: tp.Dict[str, tp.Any] = {}
 
 
 class SubgraphError(BaseModel):
@@ -54,54 +22,156 @@ class SubgraphIndexingStatus(BaseModel):
     latest_block: int
     entities: int
     error: tp.Union[SubgraphError, None]
+    sync_speed_per_min: tp.Union[float, None] = None
+    estimation_time_to_sync_chain: tp.Union[int, None] = None
 
+    def __repr_str__(self, join_str: str) -> str:
+        result = f'Synced: {self.synced}\n'
+        result += f'Head block: {self.head_block}\n'
+        result += f'Latest Block: {self.latest_block}\n'
+        result += f'Stored entities: {self.entities}\n'
+        result += f'Error: {self.error}\n'
+        result += f'Sync speed blocks/per min: {self.sync_speed_per_min}\n'
+        result += f'Estimation time to sync all blocks: {self.estimation_time_to_sync_chain} days\n'
+        return f'{result}'
 
-class SubgraphsIndexingResult(BaseModel):
-    check_date: int = int(time.time())
-    subgraphs: tp.List[SubgraphIndexingStatus]
-    length: int
-
-
-class SubgraphsStat(BaseModel):
-    data: tp.List[tp.Optional[SubgraphsIndexingResult]] = []
-
-
-result = requests.post(
-    BASE_URL, json={'query': BASE_QUERY, 'variables': None})
-
-subgraphs = []
-subgraph_indexing_data = result.json()['data']['indexingStatuses']
-for subgraph in subgraph_indexing_data:
-    if subgraph['fatalError']:
-        subgraph_error = SubgraphError(
-            block_number=subgraph['fatalError']['block']['number'],
-            handler=subgraph['fatalError']['handler'],
-            message=subgraph['fatalError']['message'])
-    else:
-        subgraph_error = None
-
-    subgraph_status = SubgraphIndexingStatus(
-        name='XXX',  # TODO: Get the Name
-        hash=subgraph['subgraph'],
-        health=subgraph['health'],
-        synced=subgraph['synced'],
-        entities=subgraph['entityCount'],
-        head_block=subgraph['chains'][0]['chainHeadBlock']['number'],
-        latest_block=subgraph['chains'][0]['latestBlock']['number'],
-        network=subgraph['chains'][0]['network'],
-        node=subgraph['node'],
-        error=subgraph_error,
-    )
-    subgraphs.append(subgraph_status)
-
-result = SubgraphsIndexingResult(subgraphs=subgraphs, length=len(subgraphs))
 
 with open('./checks.json', 'r') as f:
-    try:
-        stats_data = SubgraphsStat(**json.loads(f.read()))
-    except json.decoder.JSONDecodeError:
-        stats_data = SubgraphsStat(data=[])
+    result = json.loads(f.read())
+    sorted_checks = sorted(result['data'], key=lambda d: d['check_date'])
+    a_point = sorted_checks[0]
+    b_point = sorted_checks[-1]
 
-with open('./checks.json', 'w') as f:
-    stats_data.data.append(result)
-    f.write(stats_data.json())
+    spent_time = b_point['check_date'] - a_point['check_date']
+
+    # filter subgraps
+    stopped_subgraps = {}
+    unhealthy_subgraphs = {}
+    synced_subgraps = {}
+    syncing_subgraphs = {}
+    new_deployed_subgraphs = {}
+
+    def get_subgraph_status_by_hash(
+            subgraphs: tp.List[tp.Dict[str, tp.Any]],
+            hash: str) -> tp.Union[tp.Dict[str, tp.Any], None]:
+        for subgraph in subgraphs:
+            if subgraph['hash'] == hash:
+                return subgraph
+        return None
+
+    def print_subgraps_info(
+            name_stat: str,
+            subgraps: tp.Dict[str, SubgraphIndexingStatus]) -> None:
+        print(f'##### {name_stat.upper()} SUBGRAPHS ####\n')
+        if not subgraps:
+            print('None\n')
+        for subgraph_hash in subgraps:
+            print(f'Subgraph hash is {subgraph_hash}\nDetails:')
+            print(subgraps[subgraph_hash])
+
+    def get_ids_healthy_subgrpaps(subgraphs: tp.List[tp.Any]) -> tp.List[str]:
+        result = []
+        for subgraph in subgraphs:
+            if subgraph['health'] == 'healthy':
+                result.append(subgraph['hash'])
+        return result
+
+    def get_healthy_subgraphs(
+        subgraphs: tp.List[tp.Dict[str, tp.Any]],
+        ids_for_healthy_subgraphs: tp.List[str],
+    ) -> tp.List[tp.Dict[str, tp.Any]]:
+        result = []
+        for subgraph in subgraphs:
+            if subgraph['hash'] in ids_for_healthy_subgraphs:
+                result.append(subgraph)
+        return result
+
+    # get healthy subgraphs only
+    healthy_subgraphs_ids = get_ids_healthy_subgrpaps(
+        sorted_checks[-1]['subgraphs'])
+
+    results_for_median = {}
+    for check_id in range(0, len(sorted_checks)):
+
+        try:
+            current_subgraphs_check = sorted_checks[check_id]
+            next_subgraphs_check = sorted_checks[check_id + 1]
+        except IndexError:
+            continue
+
+        # filter only healthy
+        current_subgraphs_check['subgraphs'] = get_healthy_subgraphs(
+            current_subgraphs_check['subgraphs'], healthy_subgraphs_ids)
+        next_subgraphs_check['subgraphs'] = get_healthy_subgraphs(
+            next_subgraphs_check['subgraphs'], healthy_subgraphs_ids)
+
+        # get median sync speed and estimation in days
+        spent_time = next_subgraphs_check['check_date'] - \
+            current_subgraphs_check['check_date']
+
+        # get sync speed and estimation date in days
+        for current_subgraph in current_subgraphs_check['subgraphs']:
+            for next_subgraph in next_subgraphs_check['subgraphs']:
+                if current_subgraph['hash'] != next_subgraph['hash']:
+                    continue
+
+                # get sync speed in min
+                sync_speed_per_min = ((
+                    next_subgraph['latest_block'] - current_subgraph['latest_block']) / spent_time) * 60
+
+                try:
+                    estimation_time_to_sync_chain = int(
+                        next_subgraph['head_block'] / sync_speed_per_min / 60 / 24)
+                except ZeroDivisionError:
+                    estimation_time_to_sync_chain = None
+
+                # reduse indexing error
+                if next_subgraph['hash'] not in results_for_median:
+                    results_for_median[next_subgraph['hash']] = {
+                        'sync_speed': 0,
+                        'estimation_time_to_sync_chain': 0,
+                    }
+
+                if not estimation_time_to_sync_chain:
+                    continue
+
+                # store information
+                results_for_median[next_subgraph['hash']] = {
+                    'sync_speed': int((results_for_median[next_subgraph['hash']]['sync_speed'] + sync_speed_per_min) / 2),
+                    'estimation_time_to_sync_chain': int((results_for_median[next_subgraph['hash']]['estimation_time_to_sync_chain'] + estimation_time_to_sync_chain) / 2),
+                }
+
+        # Get final results
+        final_result = []
+        for subgraph in sorted_checks[-1]['subgraphs']:
+            if subgraph['hash'] in results_for_median:
+                subgraph['sync_speed_per_min'] = results_for_median[
+                    subgraph['hash']]['sync_speed']
+                subgraph['estimation_time_to_sync_chain'] = results_for_median[
+                    subgraph['hash']]['estimation_time_to_sync_chain']
+            final_result.append(SubgraphIndexingStatus(**subgraph))
+
+        for subgraph in final_result:
+
+            # stopped
+            if subgraph.health == 'failed':
+                stopped_subgraps[subgraph.hash] = subgraph
+                continue
+
+            # synced
+            if subgraph.synced:
+                synced_subgraps[subgraph.hash] = subgraph
+                continue
+
+            # unhealthy
+            if subgraph.health == 'unhealthy':
+                unhealthy_subgraphs[subgraph.hash] = subgraph
+
+            # syncing subgraphs
+            syncing_subgraphs[subgraph.hash] = subgraph
+
+        print(f'Total subgraps count: {len(final_result)}\n')
+        print_subgraps_info('stopped', stopped_subgraps)
+        print_subgraps_info('unhealthy', unhealthy_subgraphs)
+        print_subgraps_info('synced', synced_subgraps)
+        print_subgraps_info('syncing', syncing_subgraphs)
